0c782ae7335a844b24ab2cc608b3a76a6f843957
maria.egpadre <maria.egpadre@TIT0676460W11-1>
2025-10-28 17:35:39 -0300

    Adicionar arquivos de projeto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace projetoMakers
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
